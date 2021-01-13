78590ee96329da0872a876ea001616a6be980ffd
Erlend Ellefsen <ekbe_97@hotmail.no>
2021-01-13 10:29:06 +0100

    author added

--- models/article.js
+++ models/article.js
@@ -10,6 +10,10 @@ const articleSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    author: {
+        type: String,
+        required: true
+    },
     description: {
         type: String
     },
