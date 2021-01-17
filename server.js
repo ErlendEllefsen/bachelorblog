13502d53a7d92c772d3a54a7f0fcd8c17c3e0dbd
Erlend Ellefsen <ekbe_97@hotmail.no>
2021-01-17 12:51:39 +0100

    footer in article, small other fixes and deletes

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const app = express()
 
+// Connecting to DB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
+// Get all articles
 app.get('/', async (req, res) => {
     const articles = await Article.find().sort({
         createdAt: 'desc'
@@ -18,4 +20,5 @@ app.get('/', async (req, res) => {
 })
 app.use('/articles', articleRouter)
 
+// Port
 app.listen(process.env.PORT || 8000)
